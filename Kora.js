c7b50a6d107bdb737beb6a855e1882d63a13f73c
Suleiman <Suleimanayomide716@gmail.com>
2024-10-21 10:48:33 +0100

    Update Kora.js

--- Kora.js
+++ Kora.js
@@ -2,5 +2,5 @@ const startButton = document.querySelector('.start-button');
 
 
 start-button.addEventListener('click', () => {
-    window.location.href = 'index.html';
+    window.location.href = 'Kora.html';
 });
